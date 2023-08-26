1cde830d61d7379555eb0db2eb0e77cd7ce4373e
root <root@pury.localdomain>
2023-08-26 12:33:03 +0300

    my privateprojects

---
+++ C_practice/1st.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ *main - print hello world
+ */
+
+int main(void)
+{
+	printf("Hello, World\n");
+}
---
+++ C_practice/sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *Main - prints sum of two numbers
+ *Return: sum of intergers
+ */
+
+ int main(void)
+ {
+ int a = 89;
+ int b = 9;
+ int sum = a + b;
+ 
+ printf("%d\n", sum);
+ return (0);
+ }
