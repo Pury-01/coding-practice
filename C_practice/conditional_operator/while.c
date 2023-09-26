28ba54a28753ec87f9a1f1360d27581d59ee6fa8
Pury-01 <puryokumu@gmail.com>
2023-09-26 09:43:50 +0300

    while statement

---
+++ C_practice/conditional_operator/while.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - demonstrates use of while nested loop
+ *Return: Always 0(successful)
+ */
+
+int main(void)
+{
+	int i = 0, j;
+
+	while (i < 5)
+	{
+		j = 0;
+
+		while (j < 3)
+		{
+			printf("2 ");
+			j++;
+		}
+		printf("\n");
+		i++;
+
+	}
+	return (0);
+}
+
