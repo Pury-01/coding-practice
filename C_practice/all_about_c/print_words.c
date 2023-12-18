8ebf35c1e451c33d8e377f7992f39d7902ff6ec1
Pury-01 <puryokumu@gmail.com>
2023-12-18 13:35:26 +0300

    C is cool

---
+++ C_practice/all_about_c/print_words.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - prints words to screen using printf function
+ *Return: Always (0)successful
+ */
+
+int main(void)
+{
+	printf("Hello, I am on my way to C mastery!\n");
+	return (0);
+}
+
