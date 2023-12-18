f6312e4a92e2e7e0c0602348d0d48ab624ec7c98
Pury-01 <puryokumu@gmail.com>
2023-12-18 13:46:21 +0300

    cool is C

---
+++ C_practice/all_about_c/print_words2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints words to stdout
+ *Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("Rythm and ryhme is the way to go\n");
+	printf("On my way I go, to a place only I know\n");
+	printf("Be happy now, cause tomorrow never comes\n");
+	printf("Life is so precious, to waste it on being sad\n");
+	return (0);
+}
