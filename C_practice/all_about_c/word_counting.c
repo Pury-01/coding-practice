655b98b1d4beb88e19bc3574e24923c143f3b59c
Pury-01 <puryokumu@gmail.com>
2023-12-26 15:32:55 +0300

    program that counts number of lines, words and characters

---
+++ C_practice/all_about_c/word_counting.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - counts lines, words and characters in input
+ *
+ *Return: Always 0 (success)
+ */
+
+#define IN  1 /*inside a word */
+#define OUT 0 /* outside a word */
+
+int main(void)
+{
+	int c, nl, nw, nc, state;
+
+	state = OUT;
+	nl = nw = nc = 0;
+	while ((c = getchar()) != EOF)
+	{
+		++nc;
+		if (c == '\n')
+		{
+			++nl;
+		}
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			state = OUT;
+		}
+		else if (state == OUT)
+		{
+			state = IN;
+			++nw;
+
+		}
+	}	
+	printf("%d %d %d\n", nl, nw, nc);
+	return (0);
+}
