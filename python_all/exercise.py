dc4fca4e2b7d1aa902700e4aa16e3ff3b34fe734
Pury-01 <puryokumu@gmail.com>
2024-01-05 11:05:06 +0300

    exercise using print function and string manipilation

---
+++ python_all/exercise.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print('String Manipulation Exercise')
+print('String Concetenation is done with "+" Sign')
+print('e.g. print("Hello" + "Vero")')
+print('New line to be created with a backslash and n')
