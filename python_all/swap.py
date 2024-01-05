f26ee891a77d59b252bdcac142bd5a92d9c22d91
Pury-01 <puryokumu@gmail.com>
2024-01-05 12:32:10 +0300

    swaping values in variables

---
+++ python_all/swap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# swaps two drinks coffee and tea
+
+
+blue, yellow = 'coffee', 'tea'
+
+print(f"you will find {blue} on the table and {yellow} on the shelf")
+
+temp = blue
+blue = yellow
+yellow = temp
+
+print(f"You will find {blue} on the table and {yellow} on the shelf")
